282129e5a044209b49cd4388000c2cfd10938539
yashshinde1000 <150933069+yashshinde1000@users.noreply.github.com>
2023-11-15 20:26:15 +0530

    Add files via upload

---
+++ Task_2_portfolio-main/Task_2_portfolio-main/Task_2/portfolio.js
@@ -0,0 +1,29 @@
+(function ($) {
+    "use strict";
+
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 200) {
+            $('.navbar').fadeIn('slow').css('display', 'flex');
+        } else {
+            $('.navbar').fadeOut('slow').css('display', 'none');
+        }
+    });
+
+
+    $(".navbar-nav a").on('click', function (event) {
+        if (this.hash !== "") {
+            event.preventDefault();
+
+            $('html, body').animate({
+                scrollTop: $(this.hash).offset().top - 45
+            }, 1500, 'easeInOutExpo');
+
+            if ($(this).parents('.navbar-nav').length) {
+                $('.navbar-nav .active').removeClass('active');
+                $(this).closest('a').addClass('active');
+            }
+        }
+    });
+
+
+})(jQuery);
